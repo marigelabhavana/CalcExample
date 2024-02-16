864491a7b84436e0cf90668e3efbe1b0c369a6f9
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-16 10:50:52 +0530

    third

--- Program.cs
+++ Program.cs
@@ -5,3 +5,7 @@ Console.WriteLine("CALCULATOR");
 calc.Add(101,42);
 Console.WriteLine("Multiply call");
 calc.Multi(5,5);
+Console.WriteLine("Division Call");
+calc.Div(20,6);
+Console.WriteLine("Subtraction call");
+calc.Sub(16,8);
