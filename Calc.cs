3bebb5050a1c322abec289dc36222dc98df34653
Marigela Bhavana <marigelabhavana2000@gmail.com>
2024-02-16 10:10:47 +0530

    first commit

---
+++ Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcExample
+{
+    public class Calc
+    {
+        public void Add(double n1, double n2)
+        {
+            Console.WriteLine("Result after Addition: \t" +(n1+n2));
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Result after Subtraction: \t" +(n1-n2)); 
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result after Division: \t" + (n1/n2));
+        }
+        public void Multi(double n1, double n2)
+        {
+            Console.WriteLine("Result after Multiplication: \t" +(n1*n2));
+        }
+    }
+}
